d329e038c2487a68dc15d0b645b25db40b292ee5
immadisetty-jaya-surya <suryaimmadisetty5252@gmail.com>
2024-08-16 01:53:35 +0530

    third-commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,16 @@ const Navbar = () => {
   return (
     <nav className=' bg-gray-800 px-14 py-4'>
       <div className=' container mx-auto flex justify-between items-center'>
-        <Link href='/' className='text-white text-2xl font-bold'>E-commerce</Link>
-        <Link href='/carts' className='flex items-center text-white text-2xl font-bold'>
-          <FiShoppingCart className='mr-2' />
-          Cart ({getTotalItems()}){/* -{getSubTotal()} */}
-        </Link>
-        <Link href='/checkout' className='text-white text-xl font-semibold ml-6 bg-blue-500 py-2 px-4 rounded hover:bg-blue-600 transition'>
-          Checkout
-        </Link>
+        <Link href='/' className='text-white text-3xl font-bold'>E-commerce</Link>
+        <div className='flex'>
+          <Link href='/carts' className='flex items-center text-white text-xl font-bold'>
+            <FiShoppingCart className='mr-2' />
+            Cart ({getTotalItems()}){/* -{getSubTotal()} */}
+          </Link>
+          <Link href='/checkout' className='text-white text-xl font-semibold ml-6 bg-blue-500 py-2 px-4 rounded hover:bg-blue-600 transition'>
+            Checkout
+          </Link>
+        </div>
       </div>
     </nav>
   );
